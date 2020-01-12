202a0d66c8e6a0bdf1b0091e166b9b5c2b864abb
loki <648875862@qq.com>
2020-01-12 16:37:38 +0800

    feat: duck ==> Duck

---
+++ duck/Duck.java
@@ -0,0 +1,17 @@
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    public Duck(){
+
+    }
+    public abstract void dispaly();
+    public void preformFly() {
+        flyBehavior.fly();
+    }
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
